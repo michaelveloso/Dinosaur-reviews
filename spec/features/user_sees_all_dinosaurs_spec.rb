c9244f851c43b5ca086790aaf607d5e785bb7754
Maribeth <maribethpierce@gmail.com>
2015-09-21 16:03:37 -0400

    create route and index view

---
+++ spec/features/user_sees_all_dinosaurs_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'sees index page', %Q{
+  As a user
+  I want to view an index of dinosaurs
+  So that I can learn about them
+
+  Acceptance Criteria
+  [] - User sees an index if they visit '/dinosaurs'
+  [] - user sees an index if they visit the root
+  [] - Dinosaur names are links to their show pages
+
+} do
+  scenario 'user visits index page' do
+    dino = FactoryGirl.create(:dinosaur)
+    visit dinosaurs_path
+    expect(page).to have_content(dino.name)
+  end
+
+  scenario 'user sees index on root page' do
+    dino = FactoryGirl.create(:dinosaur)
+    visit root_path
+    expect(page).to have_content(dino.name)
+  end
+
+  scenario 'user clicks dino name' do
+    dino = FactoryGirl.create(:dinosaur)
+    visit dinosaurs_path
+    click_link(dino.name)
+    expect(page).to have_content(dino.name)
+    expect(page).to have_content(dino.location_found)
+  end
+end
