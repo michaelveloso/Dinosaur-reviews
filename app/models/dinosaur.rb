7e634d86f4e21174505f0141a2e5f209ab606221
michaelveloso <mjv.audio@gmail.com>
2015-09-22 09:26:26 -0400

    adding dinosaur-user associations and factorygirl associations

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,6 +1,9 @@
 class Dinosaur < ActiveRecord::Base
+  belongs_to :user
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :location_found, presence: true
   validates :info_url, presence: true
+  validates :user_id, presence: true
 end
