52ed2f5d2bbec6df5983ea2c13ce91699d1cf9da
michaelveloso <mjv.audio@gmail.com>
2015-09-22 15:16:07 -0400

    Tests finished

--- spec/features/user_deletes_a_dinosaur_spec.rb
+++ spec/features/user_deletes_a_dinosaur_spec.rb
@@ -6,7 +6,6 @@ feature 'user can delete an existing dinosaur', %{
   So that I can get rid of bad dinosaurs
 
   Acceptance Criteria
-  [] User can delete dinosaur from index
   [] User can delete dinosaur from show page
   [] User gets confirmation if dinosaur is deleted
   [] User is taken to index after successful deletion
@@ -24,22 +23,48 @@ feature 'user can delete an existing dinosaur', %{
       click_button 'Log in'
     end
 
-    scenario "User can delete dinosaur from index" do
-      dinosaur = FactoryGirl.create(:dinosaur)
-      visit dinosaurs_path
-      click_button "Destroy this dinosaur!"
-    end
-
     scenario "User can delete dinosaur from show page" do
+      dinosaur = FactoryGirl.create(:dinosaur)
+      visit dinosaur_path(dinosaur)
+
+      click_button "Exctinctify this dinosaur!"
     end
 
     scenario "User gets confirmation when dinosaur is deleted" do
+      dinosaur = FactoryGirl.create(:dinosaur)
+      visit dinosaur_path(dinosaur)
+
+      click_button "Exctinctify this dinosaur!"
+
+      expect(page).to have_content("Dinosaur extinctified!")
     end
 
     scenario "User is taken to index when dinosaur is deleted" do
+      dinosaur = FactoryGirl.create(:dinosaur)
+      visit dinosaur_path(dinosaur)
+
+      click_button "Exctinctify this dinosaur!"
+
+      current_path.should == dinosaurs_path
+    end
+
+    scenario "Dinosaur does not show up in index after deletion" do
+      dinosaur = FactoryGirl.create(:dinosaur)
+      visit dinosaur_path(dinosaur)
+
+      click_button "Exctinctify this dinosaur!"
+
+      expect(page).to_not have_content(dinosaur.name)
     end
 
     scenario "Dinosaur is no longer in database after deletion" do
+      dinosaur = FactoryGirl.create(:dinosaur)
+      expect(dinosaur).to eq(Dinosaur.last)
+      visit dinosaur_path(dinosaur)
+
+      click_button "Exctinctify this dinosaur!"
+
+      expect(dinosaur).to_not eq(Dinosaur.last)
     end
   end
 end
