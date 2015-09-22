1ac7b29018da9aedd1328cc024ee2d738a2449c9
michaelveloso <mjv.audio@gmail.com>
2015-09-22 10:48:56 -0400

    New and create implemented with tests

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -6,4 +6,30 @@ class DinosaursController < ApplicationController
   def show
     @dinosaur = Dinosaur.find(params[:id])
   end
+
+  def new
+    @dinosaur = Dinosaur.new
+  end
+
+  def create
+    @dinosaur = Dinosaur.new(dinosaur_params)
+
+    user = FactoryGirl.create(:user)
+    @dinosaur.user = user
+
+    if @dinosaur.save
+      flash[:success] = 'Dinosaur added!'
+      redirect_to @dinosaur
+    else
+      flash[:errors] = @dinosaur.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  private
+
+  def dinosaur_params
+    params.require(:dinosaur).permit(:name, :location_found, :info_url)
+  end
+
 end
--- spec/features/user_creates_a_dinosaur_spec.rb
+++ spec/features/user_creates_a_dinosaur_spec.rb
@@ -6,11 +6,11 @@ feature 'user can create a new dinosaur', %{
   So that I can share my favorite dinosaurs
 
   Acceptance Criteria
-  [] User can navigate to new dinosaur form from index
-  [] User can view form properly
-  [] User gets confirmation if dinosaur is entered correctly
-  [] User gets errors if form is filled out incorrectly
-  [] User gets redirected to show page of created dinosaur
+  [√] User can navigate to new dinosaur form from index
+  [√] User can view form properly
+  [√] User gets confirmation if dinosaur is entered correctly
+  [√] User gets errors if form is filled out incorrectly
+  [√] User gets redirected to show page of created dinosaur
 
 } do
 
@@ -18,7 +18,7 @@ feature 'user can create a new dinosaur', %{
 
     visit root_path
     click_link "Create a dinosaur"
-    expect(page).to have_content("Create a dinosaur")
+    expect(page).to have_content("Create a Dinosaur!")
 
   end
 
@@ -26,8 +26,8 @@ feature 'user can create a new dinosaur', %{
 
     visit new_dinosaur_path
     expect(page).to have_content("Name")
-    expect(page).to have_content("Location Found")
-    expect(page).to have_content("Information Link")
+    expect(page).to have_content("Location found")
+    expect(page).to have_content("Info url")
 
   end
 
@@ -37,18 +37,18 @@ feature 'user can create a new dinosaur', %{
       dinosaur = FactoryGirl.build(:dinosaur)
       visit new_dinosaur_path
       fill_in "Name", with: dinosaur.name
-      fill_in "Location Found", with: dinosaur.location_found
-      fill_in "Information Link", with: dinosaur.info_url
+      fill_in "Location found", with: dinosaur.location_found
+      fill_in "Info url", with: dinosaur.info_url
       click_button "Create a Dinosaur!"
-      expect(page).to have_content("Dinosaur Added!")
+      expect(page).to have_content("Dinosaur added!")
     end
 
     scenario "User is taken to show page" do
       dinosaur = FactoryGirl.build(:dinosaur)
       visit new_dinosaur_path
       fill_in "Name", with: dinosaur.name
-      fill_in "Location Found", with: dinosaur.location_found
-      fill_in "Information Link", with: dinosaur.info_url
+      fill_in "Location found", with: dinosaur.location_found
+      fill_in "Info url", with: dinosaur.info_url
       click_button "Create a Dinosaur!"
       expect(page).to have_content(dinosaur.name)
       expect(page).to have_content(dinosaur.location_found)
@@ -58,8 +58,8 @@ feature 'user can create a new dinosaur', %{
       dinosaur = FactoryGirl.build(:dinosaur)
       visit new_dinosaur_path
       fill_in "Name", with: dinosaur.name
-      fill_in "Location Found", with: dinosaur.location_found
-      fill_in "Information Link", with: dinosaur.info_url
+      fill_in "Location found", with: dinosaur.location_found
+      fill_in "Info url", with: dinosaur.info_url
       click_button "Create a Dinosaur!"
       visit root_path
       expect(page).to have_content(dinosaur.name)
@@ -72,16 +72,16 @@ feature 'user can create a new dinosaur', %{
       visit new_dinosaur_path
       click_button "Create a Dinosaur!"
       expect(page).to have_content("Name can't be blank")
-      expect(page).to have_content("Location Found can't be blank")
-      expect(page).to have_content("Information Link can't be blank")
+      expect(page).to have_content("Location found can't be blank")
+      expect(page).to have_content("Info url can't be blank")
     end
 
     scenario "User stays on form page" do
       visit new_dinosaur_path
       click_button "Create a Dinosaur!"
       expect(page).to have_content("Name")
-      expect(page).to have_content("Location Found")
-      expect(page).to have_content("Information Link")
+      expect(page).to have_content("Location found")
+      expect(page).to have_content("Info url")
     end
 
   end
