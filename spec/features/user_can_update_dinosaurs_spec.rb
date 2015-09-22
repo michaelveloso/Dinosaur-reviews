2908503c181ec6a90533353ea15e635ff0fb7761
michaelveloso <mjv.audio@gmail.com>
2015-09-22 13:15:19 -0400

    adding auth in progress

--- spec/features/user_can_update_dinosaurs_spec.rb
+++ spec/features/user_can_update_dinosaurs_spec.rb
@@ -17,94 +17,111 @@ feature 'user can update a dinosaur', %{
   [] - User stays on edit page if there are errors
 
 } do
-  scenario 'show page should have a link to edit page' do
-    dinosaur = FactoryGirl.create(:dinosaur)
-    visit dinosaur_path(dinosaur)
-    click_link 'Edit this dinosaur!'
-    expect(page).to have_content("Edit this dinosaur!")
+
+  scenario "User is not signed in" do
+    visit new_user_session_path
+    expect(page).to have_content("Sign In Sign Up")
   end
 
-  scenario 'form should be displayed correctly' do
-    dinosaur = FactoryGirl.create(:dinosaur)
-    visit edit_dinosaur_path(dinosaur)
-    expect(page).to have_content("Name")
-    expect(page).to have_content("Location found")
-    expect(page).to have_content("Info url")
-  end
+  feature "User is signed in" do
 
-  scenario 'form is pre-filled with current values' do
-    dinosaur = FactoryGirl.create(:dinosaur)
-    visit edit_dinosaur_path(dinosaur)
-    expect(page).to have_content(dinosaur.name)
-    expect(page).to have_content(dinosaur.location_found)
-    expect(page).to have_content(dinosaur.info_url)
-  end
-
-  scenario 'edit page has link to show page' do
-    dinosaur = FactoryGirl.create(:dinosaur)
-    visit edit_dinosaur_path(dinosaur)
-    click_link "Back to dinosaur!"
-    current_path.should == dinosaur_path(dinosaur)
-  end
-
-  scenario 'edit page has link to index' do
-    dinosaur = FactoryGirl.create(:dinosaur)
-    visit edit_dinosaur_path(dinosaur)
-    click_link "Back to home"
-    current_path.should == dinosaurs_path
-  end
-
-  feature 'user enters information correctly' do
-
-    scenario 'user gets confirmation of update' do
-      dinosaur = FactoryGirl.create(:dinosaur)
-      visit edit_dinosaur_path(dinosaur)
-      fill_in "Name", with: "New Name"
-      fill_in "Location found", with: "Idaho"
-      fill_in "Info url", with: "www.example.com"
-      click_button "Change this dinosaur!"
-      expect(page).to have_content("Dinosaur changed!")
+    before(:each) do
+      user = FactoryGirl.create(:user)
+      visit new_user_session_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
     end
 
-    scenario 'user is taken to show page' do
+    scenario 'show page should have a link to edit page' do
       dinosaur = FactoryGirl.create(:dinosaur)
-      visit edit_dinosaur_path(dinosaur)
-      fill_in "Name", with: "New Name"
-      fill_in "Location found", with: "Idaho"
-      fill_in "Info url", with: "www.example.com"
-      click_button "Change this dinosaur!"
-      current_path.should == dinosaur_path(dinosaur)
-      expect(page).to have_content("New Name")
-      expect(page).to have_content("Idaho")
-      expect(page).to have_content("www.example.com")
+      visit dinosaur_path(dinosaur)
+      click_link 'Edit this dinosaur!'
+      expect(page).to have_content("Edit this dinosaur!")
     end
 
-  end
-
-  feature 'user enters information incorrectly' do
-
-    scenario 'user gets errors' do
+    scenario 'form should be displayed correctly' do
       dinosaur = FactoryGirl.create(:dinosaur)
       visit edit_dinosaur_path(dinosaur)
-      fill_in "Name", with: ""
-      fill_in "Location found", with: ""
-      fill_in "Info url", with: ""
-      click_button "Change this dinosaur!"
-      expect(page).to have_content("Name can't be blank")
-      expect(page).to have_content("Location found can't be blank")
-      expect(page).to have_content("Info url can't be blank")
-    end
-
-    scenario 'user stays on page' do
-      dinosaur = FactoryGirl.create(:dinosaur)
-      visit edit_dinosaur_path(dinosaur)
-      fill_in "Name", with: ""
-      fill_in "Location found", with: ""
-      fill_in "Info url", with: ""
-      click_button "Change this dinosaur!"
       expect(page).to have_content("Name")
       expect(page).to have_content("Location found")
       expect(page).to have_content("Info url")
+    end
+
+    scenario 'form is pre-filled with current values' do
+      dinosaur = FactoryGirl.create(:dinosaur)
+      visit edit_dinosaur_path(dinosaur)
+      expect(page).to have_content(dinosaur.name)
+      expect(page).to have_content(dinosaur.location_found)
+      expect(page).to have_content(dinosaur.info_url)
+    end
+
+    scenario 'edit page has link to show page' do
+      dinosaur = FactoryGirl.create(:dinosaur)
+      visit edit_dinosaur_path(dinosaur)
+      click_link "Back to dinosaur!"
+      current_path.should == dinosaur_path(dinosaur)
+    end
+
+    scenario 'edit page has link to index' do
+      dinosaur = FactoryGirl.create(:dinosaur)
+      visit edit_dinosaur_path(dinosaur)
+      click_link "Back to home"
+      current_path.should == dinosaurs_path
+    end
+
+    feature 'user enters information correctly' do
+
+      scenario 'user gets confirmation of update' do
+        dinosaur = FactoryGirl.create(:dinosaur)
+        visit edit_dinosaur_path(dinosaur)
+        fill_in "Name", with: "New Name"
+        fill_in "Location found", with: "Idaho"
+        fill_in "Info url", with: "www.example.com"
+        click_button "Change this dinosaur!"
+        expect(page).to have_content("Dinosaur changed!")
+      end
+
+      scenario 'user is taken to show page' do
+        dinosaur = FactoryGirl.create(:dinosaur)
+        visit edit_dinosaur_path(dinosaur)
+        fill_in "Name", with: "New Name"
+        fill_in "Location found", with: "Idaho"
+        fill_in "Info url", with: "www.example.com"
+        click_button "Change this dinosaur!"
+        current_path.should == dinosaur_path(dinosaur)
+        expect(page).to have_content("New Name")
+        expect(page).to have_content("Idaho")
+        expect(page).to have_content("www.example.com")
+      end
+
+    end
+
+    feature 'user enters information incorrectly' do
+
+      scenario 'user gets errors' do
+        dinosaur = FactoryGirl.create(:dinosaur)
+        visit edit_dinosaur_path(dinosaur)
+        fill_in "Name", with: ""
+        fill_in "Location found", with: ""
+        fill_in "Info url", with: ""
+        click_button "Change this dinosaur!"
+        expect(page).to have_content("Name can't be blank")
+        expect(page).to have_content("Location found can't be blank")
+        expect(page).to have_content("Info url can't be blank")
+      end
+
+      scenario 'user stays on page' do
+        dinosaur = FactoryGirl.create(:dinosaur)
+        visit edit_dinosaur_path(dinosaur)
+        fill_in "Name", with: ""
+        fill_in "Location found", with: ""
+        fill_in "Info url", with: ""
+        click_button "Change this dinosaur!"
+        expect(page).to have_content("Name")
+        expect(page).to have_content("Location found")
+        expect(page).to have_content("Info url")
+      end
     end
   end
 end
