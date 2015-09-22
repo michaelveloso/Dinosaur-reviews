ef333c17fb857b3e122502ced72b738aaae32360
Maribeth <maribethpierce@gmail.com>
2015-09-22 15:11:48 -0400

    darn hound

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -45,7 +45,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:body, :rating,
-    :dinosaur_id).merge(user_id: current_user.id)
+    params.require(:review).permit(:body, :rating, :dinosaur_id).merge(user_id: current_user.id)
   end
 end
