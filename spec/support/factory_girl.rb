684de49f43f7f6ccdcb7fc54888d718a2f68d2fb
Michelle Casey <mcasey5216>
2015-09-23 19:57:39 -0400

    testing git

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -6,10 +6,10 @@ FactoryGirl.define do
     password 'password'
     password_confirmation 'password'
   end
-
   factory :dinosaur do
     sequence(:name) { |n| "Giganotosaurus#{n}" }
     location_found "South America"
     info_url "https://www.en.wikipedia.org/wiki/Giganotosaurus"
   end
+
 end
