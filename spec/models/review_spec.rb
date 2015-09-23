1899815d71b9ec4efee3c07438178c2ba1c7def0
michaelveloso <mjv.audio@gmail.com>
2015-09-23 15:56:49 -0400

    removing cruft FOR REALZ

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -11,12 +11,3 @@ RSpec.describe Review, type: :model do
   it { should have_valid(:rating).when("1", "2", "3", "4", "5") }
   it { should_not have_valid(:rating).when(nil, '', '16', '-5', '1.1') }
 end
-
-# belongs_to :dinosaur
-# belongs_to :user
-# has_many :comments
-#
-# validates :dinosaur, presence: true
-# validates :body, presence: true
-# validates :rating, presence: true
-# validates :rating, inclusion: { in: 1..5, message: "Must be between 1 and 5" }
