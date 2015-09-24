c5ed8238930603022f56417e79af05c8f0410c95
Maribeth <maribethpierce@gmail.com>
2015-09-24 14:29:23 -0400

    the hound is nuts

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,7 +6,6 @@ class Admin::UsersController < ApplicationController
   end
 
   private
-  
   def authorize_user
     if !user_signed_in? || !current_user.admin?
       flash[:errors] = "You don't have the privileges to do that!"
