b9d4574e6f6c6e0e2ac9a6c2861b1f4e14927cce
stacy-wells <slwells918@gmail.com>
2015-09-25 14:47:00 -0400

    working branch. adding ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,21 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+$(document).ready(function(){
+
+  $('.upvote-button').on('click', function(event){
+    event.preventDefault();
+  })
+
+});
+
+
+
+  var submitNewOption = function(option) { // Function accepts the form input
+    var request = $.ajax({ // variable set to AJAX function
+      method: "POST", // Method for POSTing input data
+      url: "/new_option", // URL matches POST in server.rb
+      data: { option: option } // data uses params objects from server.rb file
+    });
+    request.success(function() { // AJAX variable tied to a success function
