5ae0f217008c3ec6ab1a5e8ee3131889a9adbce2
Michelle Casey <mcasey5216>
2015-09-28 15:43:53 -0400

    search bar added for querying dinosaurs

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -9,4 +9,8 @@ class Dinosaur < ActiveRecord::Base
   validates :info_url, presence: true
   validates :user, presence: true
   validates :description, presence: true
+
+  def self.search(query)
+    where("name like ?", "%#{query.capitalize}%")
+  end
 end
