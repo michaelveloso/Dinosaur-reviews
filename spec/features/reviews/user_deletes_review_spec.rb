43eb5c19743377509b2acc7d39054801d5907bd9
michaelveloso <mjv.audio@gmail.com>
2015-09-28 11:49:12 -0400

    admin can delete all reviews, user can delete only own reviews

--- spec/features/reviews/user_deletes_review_spec.rb
+++ spec/features/reviews/user_deletes_review_spec.rb
@@ -6,26 +6,46 @@ feature 'user deletes review', %{
   So that I can change my mind
 
   Acceptance Criteria
+  [] User can only delete own reviews
   [] User deletes review
   [] User doesn't see review after it's deleted
 
 } do
 
-  feature "User is signed in" do
+  feature "User does not own review" do
+
     before(:each) do
-      user = FactoryGirl.create(:user)
+      @user = FactoryGirl.create(:user)
       visit new_user_session_path
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
       click_button 'Log in'
     end
-    scenario "visit details page, clicks delete button" do
-      review = FactoryGirl.create(:review)
 
+    scenario "User cannot see delete button" do
+      review = FactoryGirl.create(:review)
       visit dinosaur_path(review.dinosaur)
+
+      expect(page.has_no_button?('Delete')).to eq(true)
+    end
+  end
+
+  feature "User is signed in" do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      visit new_user_session_path
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'Log in'
+
+      @review = FactoryGirl.create(:review, user: @user)
+    end
+
+    scenario "visit show page, clicks delete button" do
+      visit dinosaur_path(@review.dinosaur)
       click_button "Delete"
       expect(page).to have_content("Review deleted!")
-      expect(page).to_not have_content(review.body)
+      expect(page).to_not have_content(@review.body)
     end
   end
 end
