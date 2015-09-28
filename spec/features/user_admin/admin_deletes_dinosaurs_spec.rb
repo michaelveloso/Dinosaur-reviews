58d07ba7a795dda7812a986b796d59a1ef65c83e
michaelveloso <mjv.audio@gmail.com>
2015-09-28 11:15:59 -0400

    addressed Jarvis' comments

--- spec/features/user_admin/admin_deletes_dinosaurs_spec.rb
+++ spec/features/user_admin/admin_deletes_dinosaurs_spec.rb
@@ -17,51 +17,39 @@ feature 'admin can delete dinosaurs', %{
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     click_button 'Log in'
+    @dinosaur = FactoryGirl.create(:dinosaur)
+    visit dinosaur_path(@dinosaur)
   end
 
   scenario "Admin sees delete dinosaur button on show page" do
-    dinosaur = FactoryGirl.create(:dinosaur)
-    visit dinosaur_path(dinosaur)
-
     find_button "Exctinctify this dinosaur!"
   end
 
   scenario "After successful deletion, admin is redirected to index" do
-    dinosaur = FactoryGirl.create(:dinosaur)
-    visit dinosaur_path(dinosaur)
-
     click_button "Exctinctify this dinosaur!"
 
     expect(current_path).to eq(dinosaurs_path)
-
   end
 
   scenario "After successful deletion, admin is notified" do
-    dinosaur = FactoryGirl.create(:dinosaur)
-    visit dinosaur_path(dinosaur)
-
     click_button "Exctinctify this dinosaur!"
 
     expect(page).to have_content("Dinosaur extinctified!")
   end
 
   scenario "Dinosaur does not show up in index after deletion" do
-    dinosaur = FactoryGirl.create(:dinosaur)
-    visit dinosaur_path(dinosaur)
-
     click_button "Exctinctify this dinosaur!"
 
-    expect(page).to_not have_content(dinosaur.name)
+    expect(page).to_not have_content(@dinosaur.name)
   end
 
   scenario "Dinosaur is no longer in database after deletion" do
-    dinosaur = FactoryGirl.create(:dinosaur)
-    expect(dinosaur).to eq(Dinosaur.last)
-    visit dinosaur_path(dinosaur)
+    expect(@dinosaur).to eq(Dinosaur.last)
+    visit dinosaur_path(@dinosaur)
 
     click_button "Exctinctify this dinosaur!"
 
-    expect(dinosaur).to_not eq(Dinosaur.last)
+    expect(@dinosaur).to_not eq(Dinosaur.last)
   end
 
 end
