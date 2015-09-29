8531a58fd6ac44c9eec827e5139985101343918c
stacy-wells <slwells918@gmail.com>
2015-09-29 13:37:55 -0400

    extracted js to a seperate file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,41 +14,4 @@
 //= require jquery_ujs
 //= require foundation
 //= require_tree .
-var votes;
 $(function(){ $(document).foundation(); });
-
-$(document).ready(function(){
-
-  $('.upvote-button').on('click', function(event){
-    event.preventDefault();
-    var button = $(this);
-    votes(button, 1);
-  });
-
-  $('.downvote-button').on('click', function(event){
-    event.preventDefault();
-    var button = $(this);
-    votes(button, -1);
-  });
-
-  $('.unvote-button').on('click', function(event){
-    event.preventDefault();
-    var button = $(this);
-    votes(button, 0);
-  });
-
-
-});
-
-votes = function(button, val) {
-  $.ajax({
-    method: 'POST',
-    url: button.parent().attr('action'),
-    dataType: 'json',
-    data: {vote: {value: val}}
-  })
-  .success(function(data){
-    var score = $(button.parent().parent().children('.score'));
-    score.text(data);
-  });
-};
