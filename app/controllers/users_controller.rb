6ed5573296db91313747a2290aa53a37b6e7958b
Michelle Casey <mcasey5216>
2015-09-30 14:57:48 -0400

    style updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,50 +3,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @dinosaurs = Dinosaur.where(user: @user)
 
     if current_user != @user
       flash[:errors] = "You're not signed in as this user"
       redirect_to dinosaurs_path
     end
-  end
-
-  def edit
-    @user = User.find(params[:id])
-    if current_user != @user
-      flash[:errors] = "You're not signed in as this user"
-      redirect_to dinosaurs_path
-    end
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if current_user != @user
-      flash[:errors] = "You're not signed in as this user"
-      redirect_to dinosaurs_path
-    end
-    if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
-      redirect_to user_path(@user)
-    else
-      flash[:errors] = @user.errors.full_messages.join(', ')
-      render :edit
-    end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    if current_user != @user
-      flash[:errors] = "You're not signed in as this user"
-      redirect_to dinosaurs_path
-    end
-    @user.destroy
-    flash[:success] = "Account deleted"
-    redirect_to root_path
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email)
   end
 end
--- spec/features/reviews/user_votes_on_review_spec.rb
+++ spec/features/reviews/user_votes_on_review_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
-
+​
 feature 'user votes on a review', %{
   As a user
   I want to vote on a review
   So that I can show which review I like and dislike
-
+​
   Acceptance Criteria
   [√] User sees upvote button
   [√] User sees downvote button
@@ -12,7 +12,7 @@ feature 'user votes on a review', %{
   [√] User will see updated points
   [√] User is able to unvote
 } do
-
+​
   feature "User is signed in" do
     before(:each) do
       user = FactoryGirl.create(:user)
@@ -21,52 +21,52 @@ feature 'user votes on a review', %{
       fill_in 'Password', with: user.password
       click_button 'Log in'
     end
-
+​
     scenario "visit details page, clicks upvote button", js: true do
       review = FactoryGirl.create(:review)
-
+​
       visit dinosaur_path(review.dinosaur)
       page.find('.upvote-button').click
-
+​
       expect(page).to have_content('1')
     end
-
+​
     scenario "visit details page, clicks downvote button", js: true do
       review = FactoryGirl.create(:review)
-
+​
       visit dinosaur_path(review.dinosaur)
       page.find('.downvote-button').click
-
+​
       expect(page).to have_content('-1')
     end
-
+​
     scenario "visit details page, clicks Unvote button", js: true do
       review = FactoryGirl.create(:review)
-
+​
       visit dinosaur_path(review.dinosaur)
       page.find('.downvote-button').click
       page.find('.unvote-button').click
-
+​
       expect(page).to have_content('0')
     end
-
+​
     scenario "user can't upvote more than once", js: true do
       review = FactoryGirl.create(:review)
-
+​
       visit dinosaur_path(review.dinosaur)
       page.find('.upvote-button').click
       page.find('.upvote-button').click
-
+​
       expect(page).to have_content('1')
     end
-
+​
     scenario "user can't downvote more than once", js: true do
       review = FactoryGirl.create(:review)
-
+​
       visit dinosaur_path(review.dinosaur)
       page.find('.downvote-button').click
       page.find('.downvote-button').click
-
+​
       expect(page).to have_content('-1')
     end
   end
