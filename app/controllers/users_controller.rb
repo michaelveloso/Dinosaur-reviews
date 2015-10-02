6e513c1785df7bd1c3eb9b14932e2621c65efc9a
stacy-wells <slwells918@gmail.com>
2015-10-02 09:25:38 -0400

    doing some DRY

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @dinosaurs = Dinosaur.where(user: @user)
-
-    if current_user != @user
-      flash[:errors] = "You're not signed in as this user"
-      redirect_to dinosaurs_path
-    end
   end
 
   def destroy
